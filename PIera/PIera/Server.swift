0a21c4877cb25b266201291b400e6a2f892ed173
daniel bauman <danielbauman2000@daniels-MacBook-Air-2.local>
2017-03-08 13:03:13 -0500

    communication established

--- PIera/PIera/Server.swift
+++ PIera/PIera/Server.swift
@@ -9,14 +9,6 @@
 import Foundation
 class Server {
     func getMain() {
-        /*var request = URLRequest(url: URL(string: "http://192.168.1.245:3000")!)
-        request.httpMethod = "GET"
-        let session = URLSession.shared
-        
-        session.dataTask(with: request) {data, response, err in
-            print("Entered the completionHandler")
-            }.resume()
- */
         let urlString = URL(string: "http://192.168.1.245:3000/")
         if let url = urlString {
             let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
