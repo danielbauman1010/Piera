bdf16cf3cf03cd0fafc512425ea21ba772a6c5b3
Adam Rivkin <adamrivkin@adams-macbook.local>
2017-03-20 14:00:29 -0400

    Fixed requirement bugs.

--- PIera/PIera/TeacherMainController.swift
+++ PIera/PIera/TeacherMainController.swift
@@ -31,7 +31,7 @@ class TeacherMainController: UIViewController{
         }
     }
     
-    //Replace and reorganize
+    //Replace and reorganize 
     func filterTime(_ experiments: [Experiment], comparisonType: ComparisonResult)->[Experiment]{
         //orderedAscending - Past, orderedSame - Present, orderedDescending - Future
         return experiments.filter{($0.time?.compare(Date())) == comparisonType}
