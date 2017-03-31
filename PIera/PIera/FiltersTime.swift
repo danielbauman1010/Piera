4e05c67b135e68068c480e4328e0cf75213904dd
danielbauman <danielbauman1010@gmail.com>
2017-03-31 09:20:22 -0400

    Reverted to working version - DO NOT PULL

--- PIera/PIera/FiltersTime.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-protocol FiltersTime{
-    func filterTime(_ experiments: [Experiment], comparisonType: ComparisonResult)->[Experiment]{
-    //orderedAscending - Past, orderedSame - Present, orderedDescending - Future
-    return experiments.filter{($0.time?.compare(Date())) == comparisonType}
-    }
-}
